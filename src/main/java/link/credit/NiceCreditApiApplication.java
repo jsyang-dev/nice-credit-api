ee0977eadf03690d74c463da8b06e7723bc6101e
Jeongsu Yang <mycat83@gmail.com>
2020-09-16 22:34:55 +0900

    Initial commit

---
+++ src/main/java/link/credit/NiceCreditApiApplication.java
@@ -0,0 +1,13 @@
+package link.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceCreditApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiceCreditApiApplication.class, args);
+    }
+
+}
